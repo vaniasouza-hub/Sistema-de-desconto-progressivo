eb6ff583a79b91cebb888cea8d450990d0e1b64d
vaniasouza-hub <vania.souza.estudos@gmail.com>
2026-09-13 19:11:23 -0300

    alt

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ if valor_total_da_compra < 200.00:
 #Verifica o valor da compra para aplicar o desconto de 10%
 elif 200.00 <= valor_total_da_compra < 300.00: 
     print("Você recebeu um desconto de 10%.")
-    # Se a compra for 300 ou mais, aplica o desconto máxim0 de 15% 
+# Se a compra for 300 ou mais, aplica o desconto máxim0 de 15% 
 else:  
     print("Você recebeu um desconto de 15%.")
